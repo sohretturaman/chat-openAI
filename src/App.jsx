3feef23128c94cf4dad1579f9cb369a0f710e1a4
şöhrett <sohretturaman@gmail.com>
2024-02-29 11:00:46 +0300

    backend folder has been created to write api in node.js, folder is ignored with .gitignore

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/** @format */
+
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>HELLO world .....</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
