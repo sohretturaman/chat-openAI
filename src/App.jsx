93356b4dbd2945f5514b9c162265ea35b61a4e39
şöhrett <sohretturaman@gmail.com>
2024-03-06 22:49:37 +0300

    chatgpt clone app's uı has been completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,38 @@
 
 import { useState } from "react";
 
-import "./App.css";
-
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <h1>HELLO world .....</h1>
-      </div>
-    </>
+    <div className="app">
+      <section className="sidebar">
+        <button> + New Chat</button>
+
+        <ul className="history">
+          <li>first Chat</li>
+        </ul>
+        <nav>
+          <p>Made by Meryem Sohret</p>
+        </nav>
+      </section>
+
+      <section className="main">
+        <h1>MariaGpt</h1>
+        <ul className="feed"></ul>
+        <div className="bottomSection">
+          <div className="inputContainer">
+            <input />
+            <div id="submit">Send</div>
+          </div>
+          <p className="info">
+            chat GPT Mar 14 version. Free Reasearch Preview. Our goal is make AI
+            systems more natural and safe to interact with. Your feedback will
+            help us improve.
+          </p>
+        </div>
+      </section>
+    </div>
   );
 }
 
