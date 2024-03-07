bbc467390c803396d36f696ffea1e500d1722995
şöhrett <sohretturaman@gmail.com>
2024-03-07 12:13:40 +0300

    completed chatgpt clone app

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,111 @@
 /** @format */
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [message, setMessage] = useState("");
+  const [value, setValue] = useState("");
+  const [prevchats, setPrevChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState("");
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: value,
+      }),
+    };
 
+    try {
+      if (value.trim().length === 0) {
+        return;
+      } else {
+        console.log("!!!!sent request to api ");
+        const response = await fetch(
+          "http://localhost:8000/completions",
+          options
+        );
+        const data = await response.json();
+        console.log(data);
+        setMessage(data.choices[0].message);
+      }
+    } catch (error) {
+      console.log("an error occured on frontend", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!currentTitle && message?.content) {
+      setCurrentTitle(value);
+    }
+
+    if (currentTitle && message) {
+      setPrevChats((prevChats) => [
+        ...prevChats,
+        {
+          role: "user",
+          content: value,
+          title: currentTitle,
+        },
+        {
+          role: message.role,
+          content: message.content,
+          title: currentTitle,
+        },
+      ]);
+    }
+  }, [currentTitle, message]);
+
+  // console.log("prev chats array", prevchats);
+
+  const chatTitles = [...new Set(prevchats.map((prevChat) => prevChat.title))];
+
+  const currentChat = prevchats.filter(
+    (prevChat) => prevChat.title === currentTitle
+  );
+  const handleNewChat = () => {
+    setCurrentTitle("");
+    setMessage("");
+    setValue("");
+  };
+
+  const handleCurrentTitle = (title) => {
+    setCurrentTitle(title);
+  };
   return (
     <div className="app">
       <section className="sidebar">
-        <button> + New Chat</button>
+        <button onClick={handleNewChat}> + New Chat</button>
 
         <ul className="history">
-          <li>first Chat</li>
+          {chatTitles?.map((title, index) => (
+            <li key={index} onClick={() => handleCurrentTitle(title)}>
+              {title}
+            </li>
+          ))}
         </ul>
-        <nav>
-          <p>Made by Meryem Sohret</p>
-        </nav>
+        <nav>{!currentTitle && <p>Made by Mariam</p>}</nav>
       </section>
 
       <section className="main">
-        <h1>MariaGpt</h1>
-        <ul className="feed"></ul>
+        <h1>MariamGpt</h1>
+        <ul className="feed">
+          {currentChat?.map((prevChat, index) => (
+            <li key={index}>
+              <p className="role">{prevChat.role}</p>
+              <br />
+              <p className="content">{prevChat.content}</p>
+            </li>
+          ))}
+        </ul>
         <div className="bottomSection">
           <div className="inputContainer">
-            <input />
-            <div id="submit">Send</div>
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div onClick={getMessages} id="submit">
+              Send
+            </div>
           </div>
           <p className="info">
             chat GPT Mar 14 version. Free Reasearch Preview. Our goal is make AI
